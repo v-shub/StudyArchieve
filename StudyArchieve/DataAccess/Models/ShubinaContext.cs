cdf5632e72ff622282576ff7a4031b255e3cb6a0
dhgdg54 <149060244+dhgdg54@users.noreply.github.com>
2025-09-10 08:38:06 +0300

    base architecture

---
+++ StudyArchieve/DataAccess/Models/ShubinaContext.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Models;
+
+public partial class ShubinaContext : DbContext
+{
+    public ShubinaContext()
+    {
+    }
+
+    public ShubinaContext(DbContextOptions<ShubinaContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<AcademicYear> AcademicYears { get; set; }
+
+    public virtual DbSet<Author> Authors { get; set; }
+
+    public virtual DbSet<Solution> Solutions { get; set; }
+
+    public virtual DbSet<SolutionFile> SolutionFiles { get; set; }
+
+    public virtual DbSet<Subject> Subjects { get; set; }
+
+    public virtual DbSet<Tag> Tags { get; set; }
+
+    public virtual DbSet<Task> Tasks { get; set; }
+
+    public virtual DbSet<TaskFile> TaskFiles { get; set; }
+
+    public virtual DbSet<TaskType> TaskTypes { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server= shubina.mssql.somee.com ;Database= shubina;User Id= v_shub_SQLLogin_1;Password= xLy-9UQ-nkT-8dW;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<AcademicYear>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__academic__3213E83F1B6CA0E6");
+
+            entity.ToTable("academic_years");
+
+            entity.HasIndex(e => e.YearLabel, "UQ__academic__588C243AB9AC4808").IsUnique();
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.YearLabel)
+                .HasMaxLength(9)
+                .HasColumnName("year_label");
+        });
+
+        modelBuilder.Entity<Author>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__authors__3213E83F90580668");
+
+            entity.ToTable("authors");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Name)
+                .HasMaxLength(255)
+                .HasColumnName("name");
+        });
+
+        modelBuilder.Entity<Solution>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__solution__3213E83F48212A39");
+
+            entity.ToTable("solutions");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.DateAdded)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnName("date_added");
+            entity.Property(e => e.IsOriginal).HasColumnName("is_original");
+            entity.Property(e => e.SolutionText)
+                .HasColumnType("text")
+                .HasColumnName("solution_text");
+            entity.Property(e => e.TaskId).HasColumnName("task_id");
+
+            entity.HasOne(d => d.Task).WithMany(p => p.Solutions)
+                .HasForeignKey(d => d.TaskId)
+                .HasConstraintName("FK_Solutions_Task");
+        });
+
+        modelBuilder.Entity<SolutionFile>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__solution__3213E83FA52D0F06");
+
+            entity.ToTable("solution_files");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.FileName)
+                .HasMaxLength(255)
+                .HasColumnName("file_name");
+            entity.Property(e => e.FilePath)
+                .HasMaxLength(500)
+                .HasColumnName("file_path");
+            entity.Property(e => e.SolutionId).HasColumnName("solution_id");
+
+            entity.HasOne(d => d.Solution).WithMany(p => p.SolutionFiles)
+                .HasForeignKey(d => d.SolutionId)
+                .HasConstraintName("FK_SolutionFiles_Solution");
+        });
+
+        modelBuilder.Entity<Subject>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__subjects__3213E83F4FAD094E");
+
+            entity.ToTable("subjects");
+
+            entity.HasIndex(e => e.Name, "UQ__subjects__72E12F1B80E3E474").IsUnique();
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Name)
+                .HasMaxLength(255)
+                .HasColumnName("name");
+        });
+
+        modelBuilder.Entity<Tag>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__tags__3213E83FCA339F7A");
+
+            entity.ToTable("tags");
+
+            entity.HasIndex(e => e.Name, "UQ__tags__72E12F1B5021F2F2").IsUnique();
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Name)
+                .HasMaxLength(100)
+                .HasColumnName("name");
+        });
+
+        modelBuilder.Entity<Task>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__tasks__3213E83F35905B03");
+
+            entity.ToTable("tasks");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.AcademicYearId).HasColumnName("academic_year_id");
+            entity.Property(e => e.ConditionText)
+                .HasColumnType("text")
+                .HasColumnName("condition_text");
+            entity.Property(e => e.DateAdded)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnName("date_added");
+            entity.Property(e => e.SubjectId).HasColumnName("subject_id");
+            entity.Property(e => e.Title)
+                .HasMaxLength(500)
+                .HasColumnName("title");
+            entity.Property(e => e.TypeId).HasColumnName("type_id");
+
+            entity.HasOne(d => d.AcademicYear).WithMany(p => p.Tasks)
+                .HasForeignKey(d => d.AcademicYearId)
+                .HasConstraintName("FK_Tasks_AcademicYear");
+
+            entity.HasOne(d => d.Subject).WithMany(p => p.Tasks)
+                .HasForeignKey(d => d.SubjectId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Tasks_Subject");
+
+            entity.HasOne(d => d.Type).WithMany(p => p.Tasks)
+                .HasForeignKey(d => d.TypeId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Tasks_Type");
+
+            entity.HasMany(d => d.Authors).WithMany(p => p.Tasks)
+                .UsingEntity<Dictionary<string, object>>(
+                    "TaskAuthor",
+                    r => r.HasOne<Author>().WithMany()
+                        .HasForeignKey("AuthorId")
+                        .OnDelete(DeleteBehavior.ClientSetNull)
+                        .HasConstraintName("FK_TaskAuthors_Author"),
+                    l => l.HasOne<Task>().WithMany()
+                        .HasForeignKey("TaskId")
+                        .HasConstraintName("FK_TaskAuthors_Task"),
+                    j =>
+                    {
+                        j.HasKey("TaskId", "AuthorId").HasName("PK_TaskAuthors");
+                        j.ToTable("task_authors");
+                        j.IndexerProperty<int>("TaskId").HasColumnName("task_id");
+                        j.IndexerProperty<int>("AuthorId").HasColumnName("author_id");
+                    });
+
+            entity.HasMany(d => d.Tags).WithMany(p => p.Tasks)
+                .UsingEntity<Dictionary<string, object>>(
+                    "TaskTag",
+                    r => r.HasOne<Tag>().WithMany()
+                        .HasForeignKey("TagId")
+                        .OnDelete(DeleteBehavior.ClientSetNull)
+                        .HasConstraintName("FK_TaskTags_Tag"),
+                    l => l.HasOne<Task>().WithMany()
+                        .HasForeignKey("TaskId")
+                        .HasConstraintName("FK_TaskTags_Task"),
+                    j =>
+                    {
+                        j.HasKey("TaskId", "TagId").HasName("PK_TaskTags");
+                        j.ToTable("task_tags");
+                        j.IndexerProperty<int>("TaskId").HasColumnName("task_id");
+                        j.IndexerProperty<int>("TagId").HasColumnName("tag_id");
+                    });
+        });
+
+        modelBuilder.Entity<TaskFile>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__task_fil__3213E83F73F69B58");
+
+            entity.ToTable("task_files");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.FileName)
+                .HasMaxLength(255)
+                .HasColumnName("file_name");
+            entity.Property(e => e.FilePath)
+                .HasMaxLength(500)
+                .HasColumnName("file_path");
+            entity.Property(e => e.TaskId).HasColumnName("task_id");
+
+            entity.HasOne(d => d.Task).WithMany(p => p.TaskFiles)
+                .HasForeignKey(d => d.TaskId)
+                .HasConstraintName("FK_TaskFiles_Task");
+        });
+
+        modelBuilder.Entity<TaskType>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__task_typ__3213E83F1AADA2B1");
+
+            entity.ToTable("task_types");
+
+            entity.HasIndex(e => e.Name, "UQ__task_typ__72E12F1B80BCA519").IsUnique();
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Name)
+                .HasMaxLength(100)
+                .HasColumnName("name");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ StudyArchieve/DataAccess/Models/TaskType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataAccess.Models;
+
+public partial class TaskType
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public virtual ICollection<Task> Tasks { get; set; } = new List<Task>();
+}
