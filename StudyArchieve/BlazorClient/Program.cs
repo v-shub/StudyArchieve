dcbf4de5a9aa1a8b7cc57ae290fc30cbef8bb9b7
v-shub <victoriya.shubina.mex@gmail.com>
2025-11-13 15:39:15 +0300

    Update Program.cs

--- StudyArchieve/BlazorClient/Program.cs
+++ StudyArchieve/BlazorClient/Program.cs
@@ -7,6 +7,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddScoped(sp =>
+    new HttpClient{});
 builder.Services.AddScoped<ApiService>();
 builder.Services.AddScoped<FileUploadService>();
 
